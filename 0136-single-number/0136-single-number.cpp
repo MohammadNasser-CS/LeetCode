e39581a3bd1d37d0610d21633de2e11101a02b60
Mohammad-Nasser <103335760+MohammadNasser-CS@users.noreply.github.com>
2024-08-27 20:35:55 +0300

    Time: 15 ms (52.64%), Space: 19.6 MB (60.36%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int singleNum=nums[0],i=0,j=1;
-        bool flag=false;
-        for(int i=0;i<nums.size();i++){
-            flag=false;
-             for(int j=0;j<nums.size();j++){
-            if(nums[i]==nums[j]&&i!=j){
-             flag=true;
-             }
-            }
-            if(!flag){
-                return nums[i];
-            }
+        // Best Solution:
+        int singleNum = 0;
+        for (int num : nums) {
+            singleNum ^= num;  // XOR all numbers
         }
-        return nums[0];
+        return singleNum;
+        // first Solution:
+        // int singleNum=nums[0],i=0,j=1;
+        // bool flag=false;
+        // for(int i=0;i<nums.size();i++){
+        //     flag=false;
+        //      for(int j=0;j<nums.size();j++){
+        //     if(nums[i]==nums[j]&&i!=j){
+        //      flag=true;
+        //      }
+        //     }
+        //     if(!flag){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[0];
        
     }
 };
