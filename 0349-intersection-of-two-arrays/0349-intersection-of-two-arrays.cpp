e7bffcbd10453cd2a892f733e3686fabdff077a2
Mohammad-Nasser <103335760+MohammadNasser-CS@users.noreply.github.com>
2024-08-28 14:42:28 +0300

    Time: 5 ms (43.88%), Space: 13.9 MB (28.38%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-     vector<int> result;
+        // best Efficient Solution
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> result;
         
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] == nums2[j]) {
-                    /*
-                    * Check if the element is already in the result.
-                    * If the value is found in the sequence, the iterator to its position is returned.
-                    * If the value is not found, the iterator to the last position is returned.
-                    */
-                    if (find(result.begin(), result.end(), nums1[i]) == result.end()) {
-                        result.push_back(nums1[i]);
-                    }
-                }
+        for (int num : nums2) {
+            if (set1.find(num) != set1.end()) {
+                result.insert(num);
             }
         }
         
-        return result;
+        return vector<int>(result.begin(), result.end());
+    // less Efficient Solution
+    //  vector<int> result;
+    //     for (int i = 0; i < nums1.size(); i++) {
+    //         for (int j = 0; j < nums2.size(); j++) {
+    //             if (nums1[i] == nums2[j]) {
+    //                 /*
+    //                 * Check if the element is already in the result.
+    //                 * If the value is found in the sequence, the iterator to its position is returned.
+    //                 * If the value is not found, the iterator to the last position is returned.
+    //                 */
+    //                 if (find(result.begin(), result.end(), nums1[i]) == result.end()) {
+    //                     result.push_back(nums1[i]);
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return result;
     }
 };
